e7d038aed793fc40a72e766887d4e5bd4c41a55a
micmine <micmine4@gmail.com>
2023-05-05 20:33:25 +0200

    Fix multy module projects

--- src/main/java/com/github/micmine/loganaintelij/LoadLoganaReportAction.java
+++ src/main/java/com/github/micmine/loganaintelij/LoadLoganaReportAction.java
@@ -4,7 +4,11 @@ import com.github.micmine.loganaintelij.controller.ErrorController;
 import com.github.micmine.loganaintelij.model.LoganaMessage;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.FileIndexFacade;
+import com.intellij.openapi.vfs.VirtualFile;
 
 import java.nio.file.Path;
 import java.util.List;
@@ -16,21 +20,14 @@ public class LoadLoganaReportAction extends AnAction {
     @Override
     public void actionPerformed(@org.jetbrains.annotations.NotNull AnActionEvent e) {
         Project project = e.getProject();
-        String projectDirectory = getProjectDirectory(project.getBasePath());
+        VirtualFile selected = FileEditorManager.getInstance(project).getSelectedEditor().getFile();
+        Module module = FileIndexFacade.getInstance(project).getModuleForFile(selected);
 
-        List<LoganaMessage> messages = parser.parseReportFile(Path.of(projectDirectory));
+        // .iml file to project directory
+        Path projectDirectory = Path.of(module.getModuleFilePath()).getParent();
+
+        List<LoganaMessage> messages = parser.parseReportFile(projectDirectory);
 
         ErrorController.getInstance().update(project, messages);
-    }
-
-    private String getProjectDirectory(String basePath) {
-        int idea = basePath.indexOf(".idea");
-
-        if (idea == -1) {
-            return basePath;
-        }
-
-        return basePath.substring(0, idea - 1);
-
     }
 }
